532091d054597ef5402f9df3aa06657cc3ea4cd1
Wai Khai Sheng <waikhaisheng@gmail.com>
2020-07-14 11:39:16 +0800

    [Redis] - init set get

---
+++ ReadMe.txt
@@ -0,0 +1,24 @@
+Install Package:
+https://github.com/microsoftarchive/redis/releases
+****************************************************************
+Port: 6379
+****************************************************************
+CMD command: visit https://redis.io/commands
+>redis-cli
+>ping
+>clear
+>set <Key> <Value>
+>get <Key>
+>expire <Key> <second>
+>keys *
+>keys k*
+>config get *
+>config set requirepass "<anyname>"
+>auth "<anyname>"
+>info
+>client list
+>monitor
+****************************************************************
+Install NuGet:
+StackExchange.Redis
+****************************************************************
---
+++ RedisCache/Program.cs
@@ -0,0 +1,32 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisCache
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
+            IDatabase db = redis.GetDatabase();
+
+            string key = string.Format("key{0}", new Random().Next(1, 100000).ToString());
+            SetKeyValue(db, key, string.Format("Value{0}", key));
+            GetKeyValue(db, key);
+
+            Console.WriteLine("Hello World!");
+        }
+
+        static void GetKeyValue(IDatabase db, string key)
+        {
+            var val = db.StringGet(key);
+            Console.WriteLine(val);
+        }
+
+        static void SetKeyValue(IDatabase db, string key, string value)
+        {
+            var success = db.StringSet(key, value);
+            Console.WriteLine(success);
+        }
+    }
+}
